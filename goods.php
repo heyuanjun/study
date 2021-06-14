15106b8fce240299de6a9d712f37957d406bb772
六六和团子 <286608747@qq.com>
2021-06-14 19:10:28 +0800

    first

---
+++ goods.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User=> HeYuanJun
+ * Date=> 2021/6/13
+ * Time=> 21=>45
+ */
+
+$arr = [
+  [
+    'name' => 'oldSix', 'price' => 1000, 'num' => 1
+  ], [
+    'name' => 'tt', 'price' => 800, 'num' => 1
+  ], [
+    'name' => 'switch', 'price' => 200, 'num' => 2
+  ], [
+    'name' => 'iphone', 'price' => 600, 'num' => 4
+  ],
+];
+
+echo json_encode($arr);
